44690d2a17d417f14b26182aab64385c01322b16
Casey Carroll <caseydean8@gmail.com>
2019-09-21 18:40:42 -0400

    initial get route established

--- server.js
+++ server.js
@@ -1,36 +1,82 @@
+var express = require("express");
+var mongojs = require("mongojs");
+
 // Parses HTML to find elements
 const cheerio = require("cheerio");
 
 // Makes HTTP request for HTML page
 const axios = require("axios");
 
-// First, tell the console what server.js is doing
-console.log("*** ENGAGE WIKILEAKS SCRAPETRON ***");
+// Initialize Express
+var app = express();
 
-// Making a request via axios for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
+// Database configuration
+var databaseUrl = "wiki-scraper";
+var collections = ["scrapedData"];
 
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Retrieve data from the db
+app.get("/", function(req, res) {
+    // Find all results from the scrapedData collection in the db
+    db.scrapedData.find({}, function(error, found) {
+      // Throw any errors to the console
+      if (error) {
+        console.log(error);
+      }
+      // If there are no errors, send the data to the browser as json
+      else {
+        res.json(found);
+      }
+    });
+  });
+
+
+
+  app.get("/scrape", function(req, res) {
+// lmk what server.js is up to
+    console.log("*** ENGAGE WIKILEAKS SCRAPETRON ***");
 // axios request for wikileaks news page
-axios.get("https://wikileaks.org/-News-.html").then((response)=> {
+    axios.get("https://wikileaks.org/-News-.html").then((response)=> {
 
   // Load the HTML into cheerio and save it to a const variable
   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  const $ = cheerio.load(response.data);
-
-  const results = [];
-
-  $("li").each((i, element) => {
-
-    const title = $(element).children(".title").text();
-
-    const link = $(element).children().find("a").attr("href");
-
-    const date = $(element).children(".timestamp").text().trim();
+    const $ = cheerio.load(response.data);
 
 
-    const summary = $(element).children().find("p").text().trim();
-    
-    results.push({ title, link, date, summary });
-  });
+        $("li").each((i, element) => {
 
-  console.log(results);
+            const title = $(element).children(".title").text();
+
+            const link = $(element).children().find("a").attr("href");
+
+            const date = $(element).children(".timestamp").text().trim();
+
+            const summary = $(element).children().find("p").text().trim();
+
+            db.scrapedData.insert({
+                title,
+                link,
+                date,
+                summary
+            },
+            function(err, inserted) {
+                if (err) {
+                    console.log(err);
+                } 
+                else {
+                    console.log(inserted);
+                }
+            });
+        });
+    res.send("scrape complete")
+    });
 });
+
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
+  });
