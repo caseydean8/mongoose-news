06483724f46940319d4b8c1029790a82d6b47fc9
Casey Carroll <caseydean8@gmail.com>
2019-09-29 21:39:21 -0400

    changed port from static to process.env

--- server.js
+++ server.js
@@ -153,6 +153,6 @@ app.get("/articles/:id", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-    console.log("App running on port 3000!");
-  });
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
